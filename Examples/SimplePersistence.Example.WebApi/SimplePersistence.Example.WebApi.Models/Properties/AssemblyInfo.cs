f69cfa65d3b328fc525e1422856155f956f79423
João Simões <nogravity00@outlook.com>
2015-06-28 13:03:12 +0100

    Added models project

---
+++ Examples/SimplePersistence.Example.WebApi/SimplePersistence.Example.WebApi.Models/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimplePersistence.Example.WebApi.Models")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("c8bae66b-ffc4-41f6-864a-2cd38de2d768")]
