ea73f225df7b59fd208dd550d276dd9dd4dbbb55
João Simões <nogravity00@outlook.com>
2015-06-28 13:13:48 +0100

    Added project for UoW interfaces

---
+++ Examples/SimplePersistence.Example.WebApi/SimplePersistence.Example.WebApi.UoW/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimplePersistence.Example.WebApi.UoW")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("4c889ed5-6362-43f4-b634-8e3185c4b99b")]
