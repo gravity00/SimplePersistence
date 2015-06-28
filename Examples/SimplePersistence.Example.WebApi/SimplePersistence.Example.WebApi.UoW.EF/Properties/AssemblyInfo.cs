ca55ddba2e31e47eed922ffebc552e5ad33a953a
João Simões <nogravity00@outlook.com>
2015-06-28 13:15:57 +0100

    Added project for UoW implementation using EF

---
+++ Examples/SimplePersistence.Example.WebApi/SimplePersistence.Example.WebApi.UoW.EF/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimplePersistence.Example.WebApi.UoW.EF")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("2b130345-f2e4-4772-b0d4-4212c313ba1b")]
