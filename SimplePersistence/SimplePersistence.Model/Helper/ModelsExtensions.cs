b67b8c43c250fc52ec16303f256e381b2e0c5d0c
João Simões <nogravity00@outlook.com>
2015-06-29 23:45:01 +0100

    Added model helper extensions

---
+++ SimplePersistence/SimplePersistence.Model/Helper/ModelsExtensions.cs
@@ -0,0 +1,58 @@
+﻿namespace SimplePersistence.Model.Helper
+{
+    using System;
+
+    /// <summary>
+    /// Models extension methods
+    /// </summary>
+    public static class ModelsExtensions
+    {
+        /// <summary>
+        /// Fills all the created metadata of a given <see cref="IHaveCreatedMeta{TCreatedBy}"/>
+        /// </summary>
+        /// <param name="entity">The entity to fill</param>
+        /// <param name="by">The by of whom created the entity</param>
+        /// <param name="on">The <see cref="DateTimeOffset"/> when it was created. If null <see cref="DateTimeOffset.Now"/> will be used</param>
+        /// <typeparam name="T">The entity type</typeparam>
+        /// <returns>The received entity after changes</returns>
+        public static T SetInitialMeta<T>(this T entity, string @by = null, DateTimeOffset? @on = null) 
+            where T : IHaveCreatedMeta<string>
+        {
+            entity.CreatedOn = @on ?? DateTimeOffset.Now;
+            entity.CreatedBy = @by;
+            return entity;
+        }
+
+        /// <summary>
+        /// Fills all the created metadata of a given <see cref="IHaveCreatedMeta{TCreatedBy}"/>
+        /// </summary>
+        /// <param name="entity">The entity to fill</param>
+        /// <param name="by">The by of whom created the entity</param>
+        /// <param name="on">The <see cref="DateTimeOffset"/> when it was created. If null <see cref="DateTimeOffset.Now"/> will be used</param>
+        /// <typeparam name="T">The entity type</typeparam>
+        /// <returns>The received entity after changes</returns>
+        public static T SetInitialMeta<T>(this T entity, string @by = null, DateTimeOffset? @on = null)
+            where T : IHaveCreatedMeta<string>, IHaveUpdatedMeta<string>
+        {
+            @on = @on ?? DateTimeOffset.Now;
+            entity.CreatedOn = entity.UpdatedOn = @on.Value;
+            entity.CreatedBy = entity.UpdatedBy = @by;
+            return entity;
+        }
+
+        /// <summary>
+        /// Fills all the created metadata of a given <see cref="IHaveUpdatedMeta{TUpdatedBy}"/>
+        /// </summary>
+        /// <param name="entity">The entity to fill</param>
+        /// <param name="username">The by of whom created the entity</param>
+        /// <param name="updatedOn">The <see cref="DateTimeOffset"/> when it was created. If null <see cref="DateTimeOffset.Now"/> will be used</param>
+        /// <typeparam name="T">The entity type</typeparam>
+        /// <returns>The received entity after changes</returns>
+        public static T FillUpdatedMeta<T>(this T entity, string username = null, DateTimeOffset? updatedOn = null) where T : IHaveUpdatedMeta<string>
+        {
+            entity.UpdatedOn = updatedOn ?? DateTimeOffset.Now;
+            entity.UpdatedBy = username;
+            return entity;
+        }
+    }
+}
