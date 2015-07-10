1de5d36f2370ffd9da9e6a27bc6ba38a52b54247
João Simões <nogravity00@outlook.com>
2015-07-11 00:33:36 +0100

    IExposeQueryable now can have more generic arguments

--- SimplePersistence/SimplePersistence.UoW/IExposeQueryable.cs
+++ SimplePersistence/SimplePersistence.UoW/IExposeQueryable.cs
@@ -53,6 +53,85 @@ namespace SimplePersistence.UoW
     /// Can be exported as a <see cref="IQueryable{TEntity}"/>
     /// </summary>
     /// <typeparam name="TEntity"></typeparam>
+    /// <typeparam name="TId01"></typeparam>
+    /// <typeparam name="TId02"></typeparam>
+    public interface IExposeQueryable<TEntity, in TId01, in TId02>
+        where TEntity : class
+        where TId01 : IEquatable<TId01>
+        where TId02 : IEquatable<TId02>
+    {
+        #region Query
+
+        /// <summary>
+        /// Gets an <see cref="IQueryable{TEntity}"/> for this repository entities
+        /// </summary>
+        /// <returns>The <see cref="IQueryable{TEntity}"/> object</returns>
+        IQueryable<TEntity> Query();
+
+        /// <summary>
+        /// Gets an <see cref="IQueryable{TEntity}"/> for this repository entities
+        /// </summary>
+        /// <param name="id01">The entity first unique identifier value</param>
+        /// <param name="id02">The entity second unique identifier value</param>
+        /// <returns>The <see cref="IQueryable{TEntity}"/> object</returns>
+        IQueryable<TEntity> QueryById(TId01 id01, TId02 id02);
+
+        /// <summary>
+        /// Gets an <see cref="IQueryable{TEntity}"/> for this repository entities
+        /// that will also fetch, on execution, all the entity navigation properties
+        /// </summary>
+        /// <param name="propertiesToFetch">The navigation properties to also fetch on query execution</param>
+        /// <returns>The <see cref="IQueryable{TEntity}"/> object</returns>
+        IQueryable<TEntity> QueryFetching(params Expression<Func<TEntity, object>>[] propertiesToFetch);
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Can be exported as a <see cref="IQueryable{TEntity}"/>
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <typeparam name="TId01"></typeparam>
+    /// <typeparam name="TId02"></typeparam>
+    /// <typeparam name="TId03"></typeparam>
+    public interface IExposeQueryable<TEntity, in TId01, in TId02, in TId03>
+        where TEntity : class
+        where TId01 : IEquatable<TId01>
+        where TId02 : IEquatable<TId02>
+        where TId03 : IEquatable<TId03>
+    {
+        #region Query
+
+        /// <summary>
+        /// Gets an <see cref="IQueryable{TEntity}"/> for this repository entities
+        /// </summary>
+        /// <returns>The <see cref="IQueryable{TEntity}"/> object</returns>
+        IQueryable<TEntity> Query();
+
+        /// <summary>
+        /// Gets an <see cref="IQueryable{TEntity}"/> for this repository entities
+        /// </summary>
+        /// <param name="id01">The entity first unique identifier value</param>
+        /// <param name="id02">The entity second unique identifier value</param>
+        /// <param name="id03">The entity third identifier value</param>
+        /// <returns>The <see cref="IQueryable{TEntity}"/> object</returns>
+        IQueryable<TEntity> QueryById(TId01 id01, TId02 id02, TId03 id03);
+
+        /// <summary>
+        /// Gets an <see cref="IQueryable{TEntity}"/> for this repository entities
+        /// that will also fetch, on execution, all the entity navigation properties
+        /// </summary>
+        /// <param name="propertiesToFetch">The navigation properties to also fetch on query execution</param>
+        /// <returns>The <see cref="IQueryable{TEntity}"/> object</returns>
+        IQueryable<TEntity> QueryFetching(params Expression<Func<TEntity, object>>[] propertiesToFetch);
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Can be exported as a <see cref="IQueryable{TEntity}"/>
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
     public interface IExposeQueryable<TEntity>
         where TEntity : class
     {
