689680cb331b505464218167273739a1cdc74452
João Simões <nogravity00@outlook.com>
2015-07-19 14:24:25 +0100

    Added logging interfaces and no operation logger

---
+++ SimplePersistence/SimplePersistence.UoW/Logging/IUoWLogger.cs
@@ -0,0 +1,102 @@
+﻿#region License
+// The MIT License (MIT)
+// Copyright (c) 2015 João Simões
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+namespace SimplePersistence.UoW.Logging
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The logger interface used by UoW classes
+    /// </summary>
+    public interface IUoWLogger
+    {
+        /// <summary>
+        /// Writes a debug message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        void Debug(string msg, Exception e = null);
+
+        /// <summary>
+        /// Writes a debug message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The task to be awaited</returns>
+        Task DebugAsync(string msg, Exception e = null, CancellationToken ct = default (CancellationToken));
+
+        /// <summary>
+        /// Writes an information message
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="e"></param>
+        void Info(string msg, Exception e = null);
+
+        /// <summary>
+        /// Writes an information message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The task to be awaited</returns>
+        Task InfoAsync(string msg, Exception e = null, CancellationToken ct = default (CancellationToken));
+
+        /// <summary>
+        /// Writes a warning message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        void Warn(string msg, Exception e = null);
+
+        /// <summary>
+        /// Writes a warning message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The task to be awaited</returns>
+        Task WarnAsync(string msg, Exception e = null, CancellationToken ct = default (CancellationToken));
+
+        /// <summary>
+        /// Writes an error message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        void Error(string msg, Exception e = null);
+
+        /// <summary>
+        /// Writes an error message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The task to be awaited</returns>
+        Task ErrorAsync(string msg, Exception e = null, CancellationToken ct = default (CancellationToken));
+
+        /// <summary>
+        /// Writes a fatal message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        void Fatal(string msg, Exception e = null);
+
+        /// <summary>
+        /// Writes a fatal message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The task to be awaited</returns>
+        Task FatalAsync(string msg, Exception e = null, CancellationToken ct = default (CancellationToken));
+    }
+}
---
+++ SimplePersistence/SimplePersistence.UoW/Logging/NopUoWLogger.cs
@@ -0,0 +1,147 @@
+﻿#region License
+// The MIT License (MIT)
+// Copyright (c) 2015 João Simões
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+namespace SimplePersistence.UoW.Logging
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Implementation of a no operation <see cref="IUnitOfWork"/>
+    /// </summary>
+    public class NopUoWLogger : IUoWLogger
+    {
+        /// <summary>
+        /// Default instance of this logger
+        /// </summary>
+        public static readonly IUoWLogger Default = new NopUoWLogger();
+
+        /// <summary>
+        /// Writes a debug message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        public void Debug(string msg, Exception e = null)
+        {
+
+        }
+
+        /// <summary>
+        /// Writes a debug message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The task to be awaited</returns>
+        public Task DebugAsync(string msg, Exception e = null, CancellationToken ct = new CancellationToken())
+        {
+            return GetNewTask(ct);
+        }
+
+        /// <summary>
+        /// Writes an information message
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="e"></param>
+        public void Info(string msg, Exception e = null)
+        {
+
+        }
+
+        /// <summary>
+        /// Writes an information message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The task to be awaited</returns>
+        public Task InfoAsync(string msg, Exception e = null, CancellationToken ct = new CancellationToken())
+        {
+            return GetNewTask(ct);
+        }
+
+        /// <summary>
+        /// Writes a warning message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        public void Warn(string msg, Exception e = null)
+        {
+
+        }
+
+        /// <summary>
+        /// Writes a warning message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The task to be awaited</returns>
+        public Task WarnAsync(string msg, Exception e = null, CancellationToken ct = new CancellationToken())
+        {
+            return GetNewTask(ct);
+        }
+
+        /// <summary>
+        /// Writes an error message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        public void Error(string msg, Exception e = null)
+        {
+
+        }
+
+        /// <summary>
+        /// Writes an error message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The task to be awaited</returns>
+        public Task ErrorAsync(string msg, Exception e = null, CancellationToken ct = new CancellationToken())
+        {
+            return GetNewTask(ct);
+        }
+
+        /// <summary>
+        /// Writes a fatal message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        public void Fatal(string msg, Exception e = null)
+        {
+
+        }
+
+        /// <summary>
+        /// Writes a fatal message
+        /// </summary>
+        /// <param name="msg">The message to write</param>
+        /// <param name="e">The exception or null if none</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The task to be awaited</returns>
+        public Task FatalAsync(string msg, Exception e = null, CancellationToken ct = new CancellationToken())
+        {
+            return GetNewTask(ct);
+        }
+
+        private static Task GetNewTask(CancellationToken ct)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            if (ct.IsCancellationRequested)
+                tcs.SetCanceled();
+            else
+                tcs.SetResult(true);
+            return tcs.Task;
+        }
+    }
+}
