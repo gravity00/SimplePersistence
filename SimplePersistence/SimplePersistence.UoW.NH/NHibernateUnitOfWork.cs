66551ea8375a3888bc5391600905d5dc17dafec8
João Simões <nogravity00@outlook.com>
2015-07-22 00:03:53 +0100

    UoW can now prepare async queryables

--- SimplePersistence/SimplePersistence.UoW.NH/NHibernateUnitOfWork.cs
+++ SimplePersistence/SimplePersistence.UoW.NH/NHibernateUnitOfWork.cs
@@ -8,6 +8,9 @@
 // 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #endregion
+
+using System.Linq;
+
 namespace SimplePersistence.UoW.NH
 {
     using System;
@@ -105,6 +108,17 @@ namespace SimplePersistence.UoW.NH
         protected override Task OnCommitAsync(CancellationToken ct)
         {
             return Task.Factory.StartNew(OnCommit, ct);
+        }
+
+        /// <summary>
+        /// Prepares a given <see cref="IQueryable{T}"/> for asynchronous work.
+        /// </summary>
+        /// <param name="queryable">The query to wrap</param>
+        /// <typeparam name="T">The query item type</typeparam>
+        /// <returns>An <see cref="IAsyncQueryable{T}"/> instance, wrapping the given query</returns>
+        public override IAsyncQueryable<T> PrepareAsyncQueryable<T>(IQueryable<T> queryable)
+        {
+            return new NHAsyncQueryable<T>(queryable);
         }
 
         #endregion
