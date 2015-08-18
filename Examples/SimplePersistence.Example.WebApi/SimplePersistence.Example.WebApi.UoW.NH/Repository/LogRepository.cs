65ba223ad7738fe1a007758607c240df4ae25ef7
João Simões <nogravity00@outlook.com>
2015-08-18 12:05:24 +0100

    Implemented repositories for NHibernate

---
+++ Examples/SimplePersistence.Example.WebApi/SimplePersistence.Example.WebApi.UoW.NH/Repository/LogRepository.cs
@@ -0,0 +1,15 @@
+﻿using NHibernate;
+using SimplePersistence.Example.WebApi.Models.Logging;
+using SimplePersistence.Example.WebApi.UoW.Repository;
+using SimplePersistence.UoW.NH;
+
+namespace SimplePersistence.Example.WebApi.UoW.NH.Repository
+{
+    public class LogRepository : NHibernateRepository<Log, long>, ILogRepository
+    {
+        public LogRepository(ISession session)
+            : base(session, (log, id) => log.Id == id)
+        {
+        }
+    }
+}
