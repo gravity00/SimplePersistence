eb13e84d02307411770ab95500b3890f5d342693
João Simões <nogravity00@outlook.com>
2015-08-25 12:14:43 +0100

    New extension methods to wrap UoW executions using the factory

--- SimplePersistence/SimplePersistence.UoW/Helper/UnitOfWorkFactoryExtensions.cs
+++ SimplePersistence/SimplePersistence.UoW/Helper/UnitOfWorkFactoryExtensions.cs
@@ -10,10 +10,150 @@
 #endregion
 namespace SimplePersistence.UoW.Helper
 {
+    using System;
+    using Exceptions;
+
     /// <summary>
     /// Extension classes for the Unit of Work factory
     /// </summary>
     public static class UnitOfWorkFactoryExtensions
     {
+        #region GetAndRelease
+
+        #region T
+
+        /// <summary>
+        /// Gets an <see cref="IUnitOfWork"/> from the given <see cref="IUnitOfWorkFactory"/> and, after 
+        /// executing the function, releases the UoW instance and returns the value
+        /// </summary>
+        /// <param name="factory">The factory to be used</param>
+        /// <param name="toExecute">The action to be executed</param>
+        /// <typeparam name="TFactory">The <see cref="IUnitOfWorkFactory"/> type</typeparam>
+        /// <typeparam name="TUoW">The <see cref="IUnitOfWork"/> type</typeparam>
+        /// <typeparam name="T">The return type</typeparam>
+        /// <exception cref="ArgumentNullException"/>
+        public static T GetAndRelease<TFactory, TUoW, T>(this TFactory factory, Func<TUoW, T> toExecute)
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            if (factory == null) throw new ArgumentNullException("factory");
+            if (toExecute == null) throw new ArgumentNullException("toExecute");
+
+            var uow = factory.Get<TUoW>();
+            try
+            {
+                return toExecute(uow);
+            }
+            finally
+            {
+                factory.Release(uow);
+            }
+        }
+
+        #endregion
+
+        #region Void
+
+        /// <summary>
+        /// Gets an <see cref="IUnitOfWork"/> from the given <see cref="IUnitOfWorkFactory"/> and, after 
+        /// executing the action, releases the UoW instance
+        /// </summary>
+        /// <param name="factory">The factory to be used</param>
+        /// <param name="toExecute">The action to be executed</param>
+        /// <typeparam name="TFactory">The <see cref="IUnitOfWorkFactory"/> type</typeparam>
+        /// <typeparam name="TUoW">The <see cref="IUnitOfWork"/> type</typeparam>
+        /// <exception cref="ArgumentNullException"/>
+        public static void GetAndRelease<TFactory, TUoW>(this TFactory factory, Action<TUoW> toExecute)
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            if (factory == null) throw new ArgumentNullException("factory");
+            if (toExecute == null) throw new ArgumentNullException("toExecute");
+
+            var uow = factory.Get<TUoW>();
+            try
+            {
+                toExecute(uow);
+            }
+            finally
+            {
+                factory.Release(uow);
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region GetAndReleaseAfterExecuteAndCommit
+
+        #region T
+
+        /// <summary>
+        /// Gets an <see cref="IUnitOfWork"/> from the given <see cref="IUnitOfWorkFactory"/> and, after 
+        /// executing the function, releases the UoW instance and returns the value
+        /// </summary>
+        /// <param name="factory">The factory to be used</param>
+        /// <param name="toExecute">The action to be executed</param>
+        /// <typeparam name="TFactory">The <see cref="IUnitOfWorkFactory"/> type</typeparam>
+        /// <typeparam name="TUoW">The <see cref="IUnitOfWork"/> type</typeparam>
+        /// <typeparam name="T">The return type</typeparam>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ConcurrencyException"/>
+        /// <exception cref="CommitException"/>
+        public static T GetAndReleaseAfterExecuteAndCommit<TFactory, TUoW, T>(this TFactory factory, Func<TUoW, T> toExecute)
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            if (factory == null) throw new ArgumentNullException("factory");
+            if (toExecute == null) throw new ArgumentNullException("toExecute");
+
+            var uow = factory.Get<TUoW>();
+            try
+            {
+                return uow.ExecuteAndCommit(toExecute);
+            }
+            finally
+            {
+                factory.Release(uow);
+            }
+        }
+
+        #endregion
+
+        #region Void
+
+        /// <summary>
+        /// Gets an <see cref="IUnitOfWork"/> from the given <see cref="IUnitOfWorkFactory"/> and, after 
+        /// executing the action, releases the UoW instance
+        /// </summary>
+        /// <param name="factory">The factory to be used</param>
+        /// <param name="toExecute">The action to be executed</param>
+        /// <typeparam name="TFactory">The <see cref="IUnitOfWorkFactory"/> type</typeparam>
+        /// <typeparam name="TUoW">The <see cref="IUnitOfWork"/> type</typeparam>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ConcurrencyException"/>
+        /// <exception cref="CommitException"/>
+        public static void GetAndReleaseAfterExecuteAndCommit<TFactory, TUoW>(this TFactory factory, Action<TUoW> toExecute)
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            if (factory == null) throw new ArgumentNullException("factory");
+            if (toExecute == null) throw new ArgumentNullException("toExecute");
+
+            var uow = factory.Get<TUoW>();
+            try
+            {
+                uow.ExecuteAndCommit(toExecute);
+            }
+            finally
+            {
+                factory.Release(uow);
+            }
+        }
+
+        #endregion
+
+        #endregion
     }
 }
