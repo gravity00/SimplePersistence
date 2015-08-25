966a95dea904712ef9d67b003752e37c6c395231
João Simões <nogravity00@outlook.com>
2015-08-25 11:44:42 +0100

    Extracted utility methods to an helper class

---
+++ SimplePersistence/SimplePersistence.UoW/Helper/UtilityExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using SimplePersistence.UoW.Properties;
+
+namespace SimplePersistence.UoW.Helper
+{
+    internal static class UtilityExtensions
+    {
+        public static Exception NewDefaultException()
+        {
+            return new Exception(Resources.TaskInFaultedStateExceptionMessage);
+        }
+
+        public static void SetExceptionFromTask<T>(this TaskCompletionSource<T> tcs, Task task)
+        {
+            if (!task.IsFaulted) return;
+
+            if (task.Exception == null) //  It should never happen
+                tcs.SetException(NewDefaultException());
+            else
+            {
+                task.Exception.Flatten().Handle(ex =>
+                {
+                    tcs.SetException(ex ?? NewDefaultException());
+                    return true;
+                });
+            }
+        }
+    }
+}
